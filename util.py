ce63e9196d77f8f8a5dc2b280a76d97348c9d273
sakib-malik <sakibmalik7417@gmail.com>
2021-07-25 16:40:16 +0530

    added examples for the filter implementation

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def dft_of_Hz(zeros, poles, A = 1, samples=500):
+def dft_of_Hz(zeros, poles, samples=500):
     '''
 
     Parameters
@@ -16,9 +16,9 @@ def dft_of_Hz(zeros, poles, A = 1, samples=500):
     Computes DFT (Discrete) of Hz from its zeros and poles
 
     '''
-    N_M = len(zeros) - len(poles)
+    N_M = len(poles) - len(zeros)
     w = np.linspace(0, 2*np.pi, samples)
-    H = A * np.exp(1j*w)**N_M
+    H = (np.exp(1j*w)**N_M)
     for z in zeros:
         H *= (np.exp(1j*w) - z)
     for p in poles:
@@ -48,8 +48,26 @@ def apply_filter(b, a, X):
     zeros = np.poly1d(b).roots
     poles = np.poly1d(a).roots
     H = dft_of_Hz(zeros, poles, samples=len(X))
-    Y = np.fft.ifft(np.fft.fft(X) * H)
-    return Y
+    Y = np.real(np.fft.ifft(np.fft.fft(X) * H))
+    return (Y / (max(Y) - min(Y))) * (max(X) - min(X))
 
-from scipy import signal
-b, a = signal.butter
+# EXAMPLES OF WORKING
+
+# N = 100
+# n = np.arange(N)
+# x = np.sin(2 * np.pi * n / N)
+# x_noise = x + np.random.normal(0, 0.05, len(x))
+# plt.plot(x_noise)
+# from scipy import signal
+# b, a = np.array([1, -1]) , np.array([2])   # HP
+# b, a = np.array([1, 1]) , np.array([2])   # LP
+# b, a = np.array([0.25, 0.25]) , np.array([1, -0.5]) # LP
+# b, a = np.array([0.25, -0.25]) , np.array([1, -0.5]) # HP
+# b, a = signal.butter(3, 0.05)
+
+# y = apply_filter(b, a, x_noise)
+# import matplotlib.pyplot as plt
+# # plt.plot(x)
+# plt.plot(x_noise)
+# plt.plot(y)
+# plt.show()
